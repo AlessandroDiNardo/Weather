d1716903ea1f2a955ff4a265a72178deceffea64
AlessandroDiNardo <alexdna003@gmail.com>
2023-06-27 09:49:46 +0200

    add packages

--- src/routes/navigator.tsx
+++ src/routes/navigator.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 // navigation
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-//screens
+// screens
 import Login from '../screens/Login';
 import Home from '../screens/Home';
 import SignIn from '../auth/SignIn'
